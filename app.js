721c3787b7256508ccb8008b16e5b2fdb2a76129
MatthewViens <matthew.j.viens@gmail.com>
2017-08-19 22:53:22 -0700

    Add momentjs for time management and add user banner to header

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const twit = require('twit');
+const moment = require('moment');
+moment().format();
 const config = require('./config');
 
 const app = express();
@@ -21,6 +23,7 @@ async function getTwitterInfo(){
   info.username = userData.screen_name;
   info.avatar = (userData.profile_image_url).replace('_normal', '');
   info.friendsCount = userData.friends_count;
+  info.banner = userData.profile_banner_url;
   timelineData = await user.get('statuses/user_timeline', {screen_name: info.username, count: 5});
   timelineData = timelineData.data;
   for (let i = 0; i < timelineData.length; i++){
@@ -35,7 +38,7 @@ async function getTwitterInfo(){
       }
       tweet.retweets = timelineData[i].retweeted_status.retweet_count;
       tweet.favorites = timelineData[i].retweeted_status.favorite_count;
-      tweet.created_at = timelineData[i].retweeted_status.created_at;
+      tweet.created_at = moment(timelineData[i].retweeted_status.created_at, 'ddd MMM DD HH:mm:ss Z YYYY').fromNow();
     } else {
       tweet.text = timelineData[i].text;
       tweet.avatar = info.avatar;
@@ -46,7 +49,7 @@ async function getTwitterInfo(){
       }
       tweet.retweets = timelineData[i].retweet_count;
       tweet.favorites = timelineData[i].favorite_count;
-      tweet.created_at = timelineData[i].created_at;
+      tweet.created_at = moment(timelineData[i].created_at, 'ddd MMM DD HH:mm:ss Z YYYY').fromNow();
     }
     info.timeline.push(tweet);
   }
@@ -67,9 +70,9 @@ async function getTwitterInfo(){
     message.name = messageData[i].sender.name;
     message.text = messageData[i].text;
     message.created_at = messageData[i].created_at;
+    message.created_at = moment(messageData[i].created_at, 'ddd MMM DD HH:mm:ss Z YYYY').fromNow()
     info.messages.push(message);
   }
-  console.log(messageData);
   return info;
 }
 
