455a2d5473f452dde3ad79bbd6c7bdcbb2d76a7b
MatthewViens <matthew.j.viens@gmail.com>
2017-08-21 16:10:39 -0700

    Restructure app, add error handling

--- app.js
+++ app.js
@@ -12,74 +12,123 @@ app.set('view engine', 'pug');
 
 const user = new twit(config);
 
-async function getTwitterInfo(){
-  const info = {};
-  info.timeline = [];
-  info.friends = [];
-  info.messages = [];
-  let userData = await user.get('account/verify_credentials', {skip_status: true});
-  userData = userData.data;
-  info.name = userData.name;
-  info.username = userData.screen_name;
-  info.avatar = (userData.profile_image_url).replace('_normal', '');
-  info.friendsCount = userData.friends_count;
-  info.banner = userData.profile_banner_url;
-  timelineData = await user.get('statuses/user_timeline', {screen_name: info.username, count: 5});
-  timelineData = timelineData.data;
-  for (let i = 0; i < timelineData.length; i++){
-    let tweet = {};
-    if(timelineData[i].retweeted_status) {
-      tweet.text = timelineData[i].retweeted_status.text;
-      tweet.avatar = (timelineData[i].retweeted_status.user.profile_image_url).replace('_normal', '');
-      tweet.name = timelineData[i].retweeted_status.user.name;
-      tweet.username = timelineData[i].retweeted_status.user.screen_name;
-      if(timelineData[i].retweeted_status.entities.media){
-        tweet.media = timelineData[i].retweeted_status.entities.media[0].media_url;
+app.use((req, res, next)=> {
+  user.get('account/verify_credentials', {skip_status: true})
+    .then((result)=> {
+      let userData = {};
+      result = result.data;
+      userData.username = result.screen_name;
+      userData.name = result.name;
+      userData.avatarURL = result.profile_image_url.replace('_normal', '');
+      userData.bannerURL = result.profile_banner_url;
+      userData.friendsCount = result.friends_count;
+      req.userData = userData;
+      next();
+    })
+    .catch((err)=> {
+      err.message = 'There was a problem getting your account information';
+      err.status = 500;
+      next(err);
+    });
+  });
+
+app.use((req, res, next)=> {
+  user.get('statuses/user_timeline', {screen_name: req.userData.username, count: 5})
+    .then((result)=> {
+      let timelineData = [];
+      for(let i = 0; i < result.data.length; i++){
+        let current = result.data[i];
+        let tweet = {};
+
+        if (current.retweeted_status) { current = current.retweeted_status }
+
+        tweet.text = current.text;
+        tweet.avatarURL = current.user.profile_image_url.replace('_normal', '');
+        tweet.name = current.user.name;
+        tweet.username = current.user.screen_name;
+        tweet.retweets = current.retweet_count;
+        tweet.favorites  = current.favorite_count;
+        tweet.created_at = moment(current.created_at, 'ddd MMM DD HH:mm:ss Z YYYY').fromNow();
+        if (current.entities.media) {
+          tweet.media = current.entities.media[0].media_url;
+        }
+        timelineData.push(tweet);
       }
-      tweet.retweets = timelineData[i].retweeted_status.retweet_count;
-      tweet.favorites = timelineData[i].retweeted_status.favorite_count;
-      tweet.created_at = moment(timelineData[i].retweeted_status.created_at, 'ddd MMM DD HH:mm:ss Z YYYY').fromNow();
-    } else {
-      tweet.text = timelineData[i].text;
-      tweet.avatar = info.avatar;
-      tweet.name = info.name;
-      tweet.username = info.username;
-      if(timelineData[i].entities.media){
-        tweet.media = timelineData[i].entities.media[0].media_url;
+      req.timelineData = timelineData;
+      next();
+    })
+    .catch((err)=> {
+      err.message = 'There was a problem getting your timeline information';
+      err.status = 500;
+      next(err);
+    });
+});
+
+app.use((req, res, next)=> {
+  user.get('friends/list', {skip_status: true, screen_name: req.userData.username, count: 5})
+    .then((result)=> {
+      let friendsData = [];
+      for(let i = 0; i < result.data.users.length; i++){
+        let current = result.data.users[i];
+        let friend = {};
+        friend.name = current.name;
+        friend.username = current.screen_name;
+        friend.avatarURL = current.profile_image_url.replace('_normal', '');
+        friendsData.push(friend);
       }
-      tweet.retweets = timelineData[i].retweet_count;
-      tweet.favorites = timelineData[i].favorite_count;
-      tweet.created_at = moment(timelineData[i].created_at, 'ddd MMM DD HH:mm:ss Z YYYY').fromNow();
-    }
-    info.timeline.push(tweet);
-  }
-  let friendsData = await user.get('friends/list', {skip_status: true, screen_name: info.username, count: 5});
-  friendsData = friendsData.data.users;
-  for(let i = 0; i < friendsData.length; i++){
-    let friend = {};
-    friend.name = friendsData[i].name;
-    friend.username = friendsData[i].screen_name;
-    friend.avatar = friendsData[i].profile_image_url.replace('_normal', '');
-    info.friends.push(friend);
-  }
-  let messageData = await user.get('direct_messages', {count: 5});
-  messageData = messageData.data;
-  for(let i = 0; i < messageData.length; i++){
-    let message = {};
-    message.avatar = messageData[i].sender.profile_image_url.replace('_normal',  '');
-    message.name = messageData[i].sender.name;
-    message.text = messageData[i].text;
-    message.created_at = messageData[i].created_at;
-    message.created_at = moment(messageData[i].created_at, 'ddd MMM DD HH:mm:ss Z YYYY').fromNow()
-    info.messages.push(message);
-  }
-  return info;
-}
+      req.friendsData = friendsData;
+      next();
+    })
+    .catch((err)=> {
+      err.message = 'There was a problem getting your friends list data';
+      err.status = 500;
+      next(err);
+    });
+});
+
+app.use((req, res, next)=> {
+  user.get('direct_messages', {count: 5})
+    .then((result)=> {
+      let messageData = [];
+
+      for(let i = 0; i < result.data.length; i++){
+        let current = result.data[i];
+        let message = {};
+        message.avatarURL = current.sender.profile_image_url.replace('_normal',  '');
+        message.name = current.sender.name;
+        message.text = current.text;
+        message.created_at = moment(current.created_at, 'ddd MMM DD HH:mm:ss Z YYYY').fromNow();
+        messageData.push(message);
+      }
+      req.messageData = messageData;
+      next();
+    })
+    .catch((err)=> {
+      err.message = 'There was a problem getting your messages information';
+      err.status = 500;
+      next(err);
+    });
+});
 
 app.get('/', (req, res) => {
-  getTwitterInfo().then(info => {
-    res.render('index', {info: info})
+  res.render('index', {
+    userData: req.userData,
+    timelineData: req.timelineData,
+    friendsData: req.friendsData,
+    messageData: req.messageData
   });
+});
+
+app.use((req, res, next)=> {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+})
+
+app.use((err, req, res, next)=> {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error', err);
 });
 
 app.listen(3000, () => {
